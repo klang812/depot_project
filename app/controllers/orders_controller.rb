dfec918aeb8ac6d787b4292bb51c9aac7f6901ae
Kerry Lang <kerrylang@colortechnology.com>
2021-10-06 14:35:45 -0700

    add react component for pay type selector

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,7 +26,6 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
-
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
